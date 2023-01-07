d5c10d91619886521ec7f61978d0d00a1a77e5dc
lucas <lucasanim12@gmail.com>
2023-01-07 12:54:06 -0300

    Dockerize msvc-courses

--- msvc-users/src/main/java/com/msvc/users/client/CourseClientRest.java
+++ msvc-users/src/main/java/com/msvc/users/client/CourseClientRest.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-courses", url = "172.17.0.1:8002")
+@FeignClient(name = "msvc-courses", url = "msvc-courses:8002")
 public interface CourseClientRest {
 
     @DeleteMapping("/delete-user/{userId}")
